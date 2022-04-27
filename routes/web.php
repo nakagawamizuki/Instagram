c91b1f8168707a16abf2230e1501776fcca7daad
nakagawamizuki <mzk821@icloud.com>
2022-04-27 17:08:00 +0900

    ログイン、ログアウト完了

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,28 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// 一般ユーザー
+Route::group(['middleware' => ['guest']], function () {
+    
+    // プレビューした瞬間の設定
+    Route::get('/', 'ToppagesController@index');
+    // ログイン認証系
+    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'Auth\LoginController@login')->name('login.post');
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-// プレビューをした瞬間の設定
-Route::get('/', 'ToppagesController@index');
+});
+
+// ユーザー認証必要
+Route::group(['middleware' => ['auth']], function () {
+    
+    // ログイン後のリダイレクト先
+    Route::get('top', function () {
+         return view('top');
+    });
+    
+    // ログアウト
+    Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+});
+
+
