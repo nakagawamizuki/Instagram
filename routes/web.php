bf33a06835a328316078f3f4b236bdd29bae92ab
nakagawamizuki <mzk821@icloud.com>
2022-05-07 09:33:24 +0900

    プロフィールモデル作成。ダミープロフィール追加

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,9 @@ Route::group(['middleware' => ['auth']], function () {
     
     // ユーザー一覧、詳細表示
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    
+    // プロフィール関係
+    Route::resource('profiles', 'ProfilesController');
 
 });
 
