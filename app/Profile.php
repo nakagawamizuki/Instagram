63e6bb49d5f0a658c0ae5f1ee2c14e4c4cc05aae
nakagawamizuki <mzk821@icloud.com>
2022-05-07 09:28:31 +0900

    プロフィールモデル作成。ダミープロフィール追加

---
+++ app/Profile.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\User; // 追加
+
+class Profile extends Model
+{
+    /**
+         * The attributes that are mass assignable.
+         *
+         * @var array
+         */
+        protected $fillable = [
+            'nickname', 'gender', 'introduction', 'image',
+        ];
+        
+        // Userモデルと1対1のリレーションを張る
+        public function user()
+        {
+            return $this->belongsTo(User::class); //hasoneとセット
+        }
+}
